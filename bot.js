b20545f19ef510fa8be7acf8285f5470ffc60c7a
Brendan Molloy <brendan@bbqsrc.net>
2015-03-22 20:03:58 +0100

    promises

--- bot.js
+++ bot.js
@@ -13,97 +13,204 @@ client = new irc.Client('irc.pirateirc.net', 'pirateint', {
     channels: channels
 });
 
-var seenPages = new Set();
-var seenPosts = new Set();
+function getRecentWikiChanges() {
+    return new Promise(function(resolve, reject) {
+        var req = request("http://wiki.pirateint.org/w/api.php?hidebots=1&days=7&limit=10&translations=filter&action=feedrecentchanges&feedformat=atom"),
+            fp = new feedparser();
 
-function getRecentWikiChanges(callback) {
-    var req = request("http://wiki.pirateint.org/w/api.php?hidebots=1&days=7&limit=10&translations=filter&action=feedrecentchanges&feedformat=atom"),
-        fp = new feedparser();
+        req.on('response', function(res) {
+            this.pipe(fp);
+        });
 
-    req.on('response', function(res) {
-        this.pipe(fp);
-    });
+        fp.on('readable', function() {
+            var items = [],
+                item;
 
-    fp.on('readable', function() {
-        var item;
-
-        while (item = this.read()) {
-            if (seenPages.has(item.guid)) {
-                continue;
+            while (item = this.read()) {
+                items.push(item);
             }
 
-            var author = item.author,
-                link = item.link,
-                title = item.title,
-                status = /oldid=0$/.test(link) ? "Page created" : "Page updated";
-
-            seenPages.add(item.guid);
-
-            if (callback != null) {
-                callback("[wiki] " + status + ": '" + title + "' (" +
-                         author + ") - " + link);
-            }
-        };
+            resolve(items);
+        });
     });
 }
 
-function getRecentForumPosts(callback) {
-    var req = request("https://discuss.pirateint.org/posts.json", function(err, res, body) {
-        var posts = JSON.parse(body).latest_posts;
+function getForumCategories() {
+    return new Promise(function(resolve, reject) {
+        var req = request("https://discuss.pirateint.org/categories.json", function(err, res, body) {
+            if (err) {
+                return reject(err);
+            }
 
-        posts.forEach(function(post) {
-           if (seenPosts.has(post.id) || post.topic_slug == null) {
-               return;
-           }
+            var categories = JSON.parse(body).category_list.categories,
+                out = Object.create(null);
 
-           seenPosts.add(post.id);
+            categories.forEach(function(category) {
+                out[category.id] = category.name;
+            });
 
-           if (callback != null) {
-               callback(post);
-           }
+            resolve(out);
         });
+    });
+}
+
+function getLatestForumTopics(callback) {
+    return new Promise(function(resolve, reject) {
+        var req = request("https://discuss.pirateint.org/latest.json", function(err, res, body) {
+            if (err) {
+                return reject(err);
+            }
+
+            var topics = JSON.parse(body).topic_list.topics,
+                out = Object.create(null);
+
+            topics.forEach(function(topic) {
+                out[topic.id] = {
+                    title: topic.title,
+                    category_id: topic.category_id
+                }
+            });
+
+            resolve(out);
+        });
+    });
+}
+
+function getRecentForumPosts() {
+    return new Promise(function(resolve, reject) {
+        var req = request("https://discuss.pirateint.org/posts.json", function(err, res, body) {
+            if (err) {
+                return reject(err);
+            }
+
+            return resolve(JSON.parse(body).latest_posts);
+        });
+    });
+}
+
+var seenPages = new Set();
+var seenPosts = new Set();
+var seenTopics;
+var seenCategories;
+
+function formatWikiPage(page) {
+    var author = page.author,
+        link = page.link,
+        title = page.title,
+        status = /oldid=0$/.test(link) ? "Page created" : "Page updated";
+
+    return ("[wiki] " + status + ": '" + title + "' (" +
+            author + ") - " + link);
+}
+
+function formatForumPost(knownTopics, knownCategories, post) {
+    let v = "";
+
+    if (knownTopics[post.topic_id]) {
+        let topic = knownTopics[post.topic_id];
+        v += "[" + knownCategories[topic.category_id] + "] ";
+        v += topic.title
+    } else { v += "[???] ???" }
+
+    v += ": <" + post.username + "> ";
+
+    let raw = post.raw;
+    if (raw.length > 70) {
+        raw = raw.slice(0, 70) + "…";
+    }
+
+    v += '"' + raw.replace(/[\r\n]/g, " ") + '" - ';
+
+    v += "https://discuss.pirateint.org/t/" +
+         post.topic_slug + "/" +
+         post.topic_id + "/" +
+         post.post_number;
+
+    return v;
+}
+
+function firstRun() {
+return Promise.all([
+        getRecentWikiChanges(),
+        getForumCategories(),
+        getLatestForumTopics(),
+        getRecentForumPosts()
+    ]).then(function(vals) {
+        let wikiChanges = vals[0];
+        let forumCats = vals[1];
+        let forumTopics = vals[2];
+        let forumPosts = vals[3];
+
+        console.log(new Date, "time for init");
+
+        wikiChanges.forEach(function(change) {
+            seenPages.add(change.guid);
+        });
+
+        forumPosts.forEach(function(post) {
+            seenPosts.add(post.id);
+        });
+
+        seenCategories = forumCats;
+        seenTopics = forumTopics;
     });
 }
 
 client.addListener('connect', function() {
-    let wikiCb = function(v) {
-        channels.forEach(function(channel) {
-            client.say(channel, v);
-        });
-    };
+    firstRun().then(function() {
+        console.log(new Date, "Init'd.");
 
-    let forumCb = function(post) {
-        let v = "[forum] @" +
-                //post.topic_slug + " - @" +
-                post.username + ": ";
+        let fmtForumPostLocal = formatForumPost.bind(null,
+                                                     seenTopics,
+                                                     seenCategories);
 
-        let raw = post.raw;
-        if (raw.length > 60) {
-            raw = raw.slice(0, 57) + "...";
-        }
+        setInterval(function() {
+            console.log(new Date, "getting new data");
 
-        v += '"' + raw + '" - ';
+            Promise.all([getRecentWikiChanges(),
+                         getLatestForumTopics(),
+                         getRecentForumPosts()]).then(function(values) {
+                let wiki = values[0];
+                let topics = values[1];
+                let posts = values[2];
 
-        v += "https://discuss.pirateint.org/t/" +
-             post.topic_slug + "/" +
-             post.topic_id + "/" +
-             post.post_number;
+                console.log(new Date, "updating.");
 
-        channels.forEach(function(channel) {
-            client.say(channel, v);
-        });
-    };
+                Object.keys(topics).forEach(function(topic) {
+                    seenTopics[topic] = topics[topic];
+                });
 
-    // Initial run.
-    console.log(new Date, "Initialising wiki change set.");
-    getRecentWikiChanges();
-    console.log(new Date, "Initialising forum change set.");
-    getRecentForumPosts();
+                posts.forEach(function(post) {
+                    if (seenPosts.has(post.id)) {
+                        return;
+                    }
 
-    setInterval(function() {
-        console.log(new Date, "Getting recent changes...");
-        getRecentWikiChanges(wikiCb);
-        getRecentForumPosts(forumCb);
-    }, 60000);
+                    let formatted = fmtForumPostLocal(post);
+                    console.log(new Date, formatted);
+
+                    channels.forEach(function(channel) {
+                        client.say(channel, formatted);
+                    });
+
+                    seenPosts.add(post.id);
+                });
+
+                wiki.forEach(function(change) {
+                    if (seenPages.has(change.guid)) {
+                        return;
+                    }
+
+                    let formatted = formatWikiPage(change);
+                    console.log(new Date, formatted);
+
+                    channels.forEach(function(channel) {
+                        client.say(channel, formatted);
+                    });
+
+                    seenPages.add(change.guid);
+                });
+            });
+        }, 60000);
+    }, function(err) { console.error(err); });
 });
 

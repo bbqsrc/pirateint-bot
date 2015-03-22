7f7f5c59510a4ee8b6fbce628ebbcc98f1a7c5aa
Brendan Molloy <brendan@bbqsrc.net>
2015-03-22 16:34:09 +0100

    forum

--- bot.js
+++ bot.js
@@ -14,8 +14,9 @@ client = new irc.Client('irc.pirateirc.net', 'pirateint', {
 });
 
 var seenPages = new Set();
+var seenPosts = new Set();
 
-function getRecentChanges(callback) {
+function getRecentWikiChanges(callback) {
     var req = request("http://wiki.pirateint.org/w/api.php?hidebots=1&days=7&limit=10&translations=filter&action=feedrecentchanges&feedformat=atom"),
         fp = new feedparser();
 
@@ -46,20 +47,63 @@ function getRecentChanges(callback) {
     });
 }
 
+function getRecentForumPosts(callback) {
+    var req = request("https://discuss.pirateint.org/posts.json", function(err, res, body) {
+        var posts = JSON.parse(body).latest_posts;
+
+        posts.forEach(function(post) {
+           if (seenPosts.has(post.id) || post.topic_slug == null) {
+               return;
+           }
+
+           seenPosts.add(post.id);
+
+           if (callback != null) {
+               callback(post);
+           }
+        });
+    });
+}
+
 client.addListener('connect', function() {
     let wikiCb = function(v) {
         channels.forEach(function(channel) {
             client.say(channel, v);
         });
-    }
+    };
+
+    let forumCb = function(post) {
+        let v = "[forum] " +
+                post.topic_slug + " - @" +
+                post.username + ": ";
+
+        let raw = post.raw;
+        if (raw.length > 60) {
+            raw = raw.slice(0, 57) + "...";
+        }
+
+        v += '"' + raw + '" - ';
+
+        v += "https://discuss.pirateint.org/t/" +
+             post.topic_slug + "/" +
+             post.topic_id + "/" +
+             post.post_number;
+
+        channels.forEach(function(channel) {
+            client.say(channel, v);
+        });
+    };
 
     // Initial run.
     console.log(new Date, "Initialising wiki change set.");
-    getRecentChanges();
+    getRecentWikiChanges();
+    console.log(new Date, "Initialising forum change set.");
+    getRecentForumPosts();
 
     setInterval(function() {
         console.log(new Date, "Getting recent changes...");
-        getRecentChanges(wikiCb);
+        getRecentWikiChanges(wikiCb);
+        getRecentForumPosts(forumCb);
     }, 60000);
 });
 
